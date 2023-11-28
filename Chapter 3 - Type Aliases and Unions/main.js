39591b22c423ecdd338f9831ca5646a5adeae02a
Aryainguz <aryaninguz369@gmail.com>
2023-11-28 10:41:34 +0530

    added Chapter 3 - Type Aliases and Unions

---
+++ Chapter 3 - Type Aliases and Unions/main.js
@@ -0,0 +1,5 @@
+// Type Aliases allow defining types with a custom name (an Alias).
+var a = 1;
+// Example 2
+// Type Union
+var b = 1;
---
+++ Chapter 3 - Type Aliases and Unions/main.ts
@@ -0,0 +1,10 @@
+// Type Aliases allow defining types with a custom name (an Alias).
+
+// Example 1
+// Type Alias
+type Combinable = number | string;
+let a: Combinable = 1;
+
+// Example 2
+// Type Union
+let b: number | string = 1;
