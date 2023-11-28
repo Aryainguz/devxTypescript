5dd9da2e3ca9fad4be17866dd4f417a36abc29fb
Aryainguz <aryaninguz369@gmail.com>
2023-11-28 09:52:49 +0530

    added code&notes on declaring variables

--- Chapter 1/main.js
+++ Chapter 1/main.js
@@ -1,2 +1,18 @@
+// Typescript is a dev tool and all typescript code is converted to js in production
+// Typescript is a superset of javascript
+// Typescript is a strongly typed language
+// Typescript is a compiled language
+// Typescript is an object oriented language\
+// Typescript is a statically typed language
+// Typescript is a transpiled language
+// Typescript is a typed superset of javascript that compiles to plain javascript
+// Two ways to declare variables in typescript with specifying types
+// 1
 var a = 1;
-console.log(a);
+//2
+var b = 1;
+// You can't change the type of a variable once it is declared
+// a = "hello"; // This will throw an error
+// You can also declare variables with the any type
+var c = 1;
+c = "hello"; // This is allowed
--- Chapter 1/main.ts
+++ Chapter 1/main.ts
@@ -1,2 +1,25 @@
-let a = <number> 1;
-console.log(a);
+// Typescript is a dev tool and all typescript code is converted to js in production
+// Typescript is a superset of javascript
+// Typescript is a strongly typed language
+// Typescript is a compiled language
+// Typescript is an object oriented language\
+// Typescript is a statically typed language
+// Typescript is a transpiled language
+// Typescript is a typed superset of javascript that compiles to plain javascript
+
+
+// Two ways to declare variables in typescript with specifying types
+
+// 1
+let a = <number>1;
+
+//2
+let b: number = 1;
+
+// You can't change the type of a variable once it is declared
+// a = "hello"; // This will throw an error
+// You can also declare variables with the any type
+
+let c: any = 1;
+
+c = "hello"; // This is allowed
